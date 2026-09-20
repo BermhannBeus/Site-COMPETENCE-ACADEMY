755d03586006fbaa368ebd17f8807ec8bcbf89b4
BermhannBeus <158602367+BermhannBeus@users.noreply.github.com>
2026-09-20 12:24:39 -0700

    Use stable Gmail SMTP port

--- Site C.A/server.js
+++ Site C.A/server.js
@@ -67,6 +67,9 @@ const clearAuthCookie = (res) => {
 // Configuration Nodemailer
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
