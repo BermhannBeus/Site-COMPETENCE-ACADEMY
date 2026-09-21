19d2ccf3fe05f6dd1ecc355d2774616022f68640
BermhannBeus <158602367+BermhannBeus@users.noreply.github.com>
2026-09-21 16:56:52 -0700

    Improve caching error handling in service worker
    Updated caching logic to handle errors during cache addition.

--- Site C.A/sw.js
+++ Site C.A/sw.js
@@ -12,7 +12,9 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        return cache.addAll(urlsToCache);
+        return Promise.all(urlsToCache.map((url) => cache.add(url).catch((error) => {
+          console.error(`Impossible de mettre en cache ${url}:`, error);
+        })));
       })
   );
 });
